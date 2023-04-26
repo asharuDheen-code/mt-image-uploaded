6abc04a1ef732f9caec1b6920bd54a42c62339c8
Asharudheen <asharuashkar@gmail.com>
2023-04-26 10:27:18 +0530

    nuwanted codes removed

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,6 @@
 const router = require("express").Router();
 const { addImage, getImage } = require("../controllers/user");
 
-// const upload = multer({ dest: "uploads/" });
-// const { protect, authorize } = require("../middleware/auth");
-
-// router.post("/create_user", protect, authorize("customer"), createUser);
-// router.post("/create_user", createUser);
-// router.post("/add_user", addUser);
-
-// router.get("/get_customers", getCustomers);
-// router.get("/get_all_customers", getAllCustomers);
-// router.delete("/delete_customer", deleteCustomer);
-
 router.post("/addimage", addImage);
 router.get("/getimage", getImage);
 
